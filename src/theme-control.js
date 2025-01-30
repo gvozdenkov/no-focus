64b70da05d07870a8e5fc3f9cccb52ed082cb8f6
arty gvozdenkov <gvozdenkov@gmail.com>
2025-01-30 23:06:55 +0300

    fix: add load saved theme

--- src/theme-control.js
+++ src/theme-control.js
@@ -2,23 +2,38 @@ export var setThemeControl = () => {
   var themeControls = document.querySelector('.theme-controls');
   var themeInputs = themeControls.querySelectorAll('.theme-controls input');
 
+  var uncheckAllInputs = (inputs) =>
+    inputs.forEach((input) => {
+      input.setAttribute('aria-checked', 'false');
+    });
+
+  var setInputChecked = (input) => input.setAttribute('aria-checked', 'true');
+
+  var findInputByValue = (inputs, value) => [...inputs].find((input) => input.value === value);
+
+  var setDocumentTheme = (theme) => document.documentElement.setAttribute('data-theme', theme);
+
   var handleSwitchTheme = (e) => {
     var activeInput = e.target;
     var theme = activeInput.value;
 
     localStorage.setItem('theme', theme);
-    document.documentElement.setAttribute('data-theme', theme);
-
-    themeInputs.forEach((input) => {
-      input.setAttribute('aria-checked', 'false');
-    });
-
-    activeInput.setAttribute('aria-checked', 'true');
+    setDocumentTheme(theme);
+    uncheckAllInputs(themeInputs);
+    setInputChecked(activeInput);
   };
 
   var loadSavedTheme = () => {
     var theme = localStorage.getItem('theme');
-    theme && document.documentElement.setAttribute('data-theme', theme);
+    if (theme) {
+      setDocumentTheme(theme);
+      uncheckAllInputs(themeInputs);
+      var activeInput = findInputByValue(themeInputs, theme);
+      setInputChecked(activeInput);
+    } else {
+      var activeInput = findInputByValue(themeInputs, 'auto');
+      setInputChecked(activeInput);
+    }
   };
 
   loadSavedTheme();
