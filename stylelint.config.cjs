9ea89d6e3f6f66c704b0d8c66944c19525135c6e
arty gvozdenkov <gvozdenkov@gmail.com>
2025-01-31 09:29:36 +0300

    ci: setup prettier, styleling & precommit hook

---
+++ stylelint.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: 'stylelint-config-standard',
+  rules: {
+    'custom-property-pattern': '_?.+',
+    'selector-class-pattern': '^([a-z][a-z0-9]*)((__|_|-|--)[a-z0-9]+)*$',
+  },
+};
